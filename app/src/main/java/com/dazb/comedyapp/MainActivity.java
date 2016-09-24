c620f1c2c5143580151badf562dddfe941d21a51
Dariush Bahri <db819@york.ac.uk>
2016-09-24 18:12:00 +0100

    Got rid of Composer nonsense for Ents24 PHP Client API. Turns out easier just to make HTTP requests by hand. Cos Java isn't PHP. It's like this whole other thing. Bit of a waste of my time if I'm honest. Also, able to get Authorization Token to make queries to Ents database. Yay.

--- app/src/main/java/com/dazb/comedyapp/MainActivity.java
+++ app/src/main/java/com/dazb/comedyapp/MainActivity.java
@@ -17,13 +17,35 @@ import com.ticketmaster.discovery.model.Date.Start;
 import com.ticketmaster.discovery.model.Event;
 import com.ticketmaster.discovery.model.Events;
 
-import java.util.List;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+    // Debug log TAG
     private static final String TAG = MainActivity.class.getSimpleName();
 
     // DazB Ticketmaster API key
     private static String apikey = System.getProperty("ticketmaster-api-key", "xoGWGgRDOLHGsutqGIk0YLGaNXaYhsAA");
+
+    // ENTS24 API keys
+    private static String entsClientID = "91fdea0e6e8de74094ad0495f34ba081903e8bea";
+    private static String entsClientSecret = "bfd40f1dcb565e9a0e206395c7ae7c6f108cd26a";
+    private static String entsUsername = "dazbahri@hotmail.co.uk";
+    private static String entsPassword = "ShitPissFuckCunt";
+    private String entsToken;
+
+
+    // Instantiate a DiscoveryApi client:
+    DiscoveryApi discoveryApi = new DiscoveryApi(apikey);
+
+    // York
     private static final String LAT = "53.9600";
     private static final String LONG = "-1.0873";
 
@@ -32,21 +54,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new EntsGetEventsTask().execute();
         new TicketmasterGetEventsTask().execute();
     }
 
     /**
      * AsyncTask that handles the retrieving of events from Ticketmaster API
-     * Params: ApiKey
+     * Params: Void
      * Progress: Void
      * Result: List of events
      */
     class TicketmasterGetEventsTask extends AsyncTask<Void, Void, PagedResponse<Events>> {
         @Override
         protected PagedResponse<Events> doInBackground(Void... params) {
-            // 1. Instantiate a DiscoveryApi client:
-            DiscoveryApi discoveryApi = new DiscoveryApi(apikey);
-            // 2. Make our first search
+            // Make our first search
             PagedResponse<Events> response = null;
             try {
                 response =
@@ -55,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                                 // TODO: way to add filter for just comedy? Cos else we have to filter through response, which isn't ideal, since suppose response don't have comedy, we get no results on UI
                         );
             } catch (Exception e) {
-                Log.d(TAG, "RetrieveDataTask, PagedResponse Error: ", e);
+                Log.d(TAG, "TicketmasterGetEventsTask, PagedResponse Error: ", e);
             }
             // If we get some sort of response, return it. Else, return null
             if (response != null) {
@@ -75,14 +96,90 @@ public class MainActivity extends AppCompatActivity {
             TextView events = (TextView)findViewById(R.id.textView);
             events.setMovementMethod(new ScrollingMovementMethod()); // Make shit scroll
 
-            // Go through every event returned
-            for(com.ticketmaster.discovery.model.Event event : response.getContent().getEvents()) {
-                // If event classified as comedy, update UI with the event name
-                // TODO: as discussed above, not ideal. Really we want only comedy events returned from Ticketmaster, not any old event
-                if (event.getClassifications().get(0).getGenre().getName().toLowerCase().contains("comedy")) {
-                    events.append(event.getName() + "\r\n");
+            if (response != null) {
+                // Go through every event returned
+                for(com.ticketmaster.discovery.model.Event event : response.getContent().getEvents()) {
+                    // If event classified as comedy, update UI with the event name
+                    // TODO: as discussed above, not ideal. Really we want only comedy events returned from Ticketmaster, not any old event
+                    if (event.getClassifications().get(0).getGenre().getName().toLowerCase().contains("comedy")) {
+                        events.append(event.getName() + "\r\n");
+                    }
                 }
             }
         }
+    }
+
+    /**
+     * AsyncTask that handles the retrieving of events from Ents24 API
+     * Params: Void
+     * Progress: Void
+     * Result: Void
+     */
+    class EntsGetEventsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Create connection
+                HttpURLConnection entsConnection = (HttpURLConnection) new URL("https://api.ents24.com/auth/login").openConnection();
+                entsConnection.setRequestMethod("POST");
+                entsConnection.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded");
+                entsConnection.setUseCaches(false);
+                entsConnection.setDoOutput(true);
+
+                // Send request
+                DataOutputStream wr = new DataOutputStream (
+                        entsConnection.getOutputStream());
+                wr.writeBytes("client_id="+ entsClientID + "&client_secret=" + entsClientSecret + "&username=" + entsUsername + "&password=" + entsPassword);
+                wr.close();
+
+                //Get Response
+                int statusCode = entsConnection.getResponseCode();
+                StringBuilder response = new StringBuilder();
+                // Check request codes for Bad request
+                if (statusCode >= 200 && statusCode < 400) {
+                    // Good request
+                    InputStream is = entsConnection.getInputStream();
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        response.append(line);
+                        response.append('\r');
+                    }
+                    rd.close();
+
+                    // Get token
+                    JSONObject responseJSON = new JSONObject(response.toString());
+                    if (responseJSON.getString("access_token") != null) {
+                        entsToken = responseJSON.getString("access_token");
+                    }
+                    else {
+                        entsToken = "";
+                    }
+
+                }
+                else {
+                    // Bad request
+                    InputStream is = entsConnection.getErrorStream();
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        response.append(line);
+                        response.append('\r');
+                    }
+                    rd.close();
+                }
+
+                Log.d(TAG, response.toString());
+
+            }
+            catch (Exception e) {
+                Log.d(TAG, "ENTS24 connection error: ", e);
+            }
+
+            return null;
+        }
+
     }
 }
